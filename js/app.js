101c4b4bfdd43ca507b98bdb5cac4221ae48e9e1
romainseb <sebastien.romain@gmail.com>
2014-08-01 00:46:59 +0200

    Add some content in home page

--- js/app.js
+++ js/app.js
@@ -1,15 +1,6 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
 var amCompanion = angular.module('amCompanion', [
-  'ngRoute','ngAnimate',"ngTouch", 'igTruncate'
+  'ngRoute','ngAnimate',"ngTouch", 'igTruncate',"matchMedia"
 ]);
-
-amCompanion.config(['$httpProvider', function ($httpProvider) {
-    //Reset headers to avoid OPTIONS request (aka preflight)
-    $httpProvider.defaults.headers.common = {};
-    $httpProvider.defaults.headers.post = {};
-    $httpProvider.defaults.headers.put = {};
-    $httpProvider.defaults.headers.patch = {};
-}]);
