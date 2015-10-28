ed2cd0b19d10382cd5da8e6900898d0a7f29a0e9
romainseb <sebastien.romain@gmail.com>
2015-10-28 13:29:12 +0100

    Update some details

--- gulpfile.js
+++ gulpfile.js
@@ -1,108 +1,105 @@
-/**
- * Created by romainseb on 02/04/15.
- */
 var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var templateCache = require('gulp-angular-templatecache');
 var concat = require('gulp-concat');
 var cssmin = require('gulp-cssmin');
+var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
-var jshint = require('gulp-jshint');
-var ngTemplates = require('gulp-ng-templates');
-var htmlmin = require('gulp-htmlmin');
-var csslint = require('gulp-csslint');
+var watch = require('gulp-watch');
+var minifyHTML = require('gulp-minify-html');
 var ngAnnotate = require('gulp-ng-annotate');
-var bower = require('gulp-bower');
-var rename = require('gulp-rename');
-var autoprefixer = require('gulp-autoprefixer');
-var plumber = require('gulp-plumber');
+var babel = require('gulp-babel');
+var gls = require('gulp-live-server');
+var mergeStream = require('merge-stream');
+var plumber = require("gulp-plumber");
 
+var paths = {
+    scriptsApplication: [
+        "app/app.js",
+        "app/**/*.js",
+        "!app/vendors/**/*.min.js"
+    ],
+    scriptsVendors: [
+        "app/vendors/js/angular.min.js",
+        "app/vendors/**/*.min.js"
+    ],
+    sassFiles: [
+        "app/**/*.scss"
+    ],
+    cssFiles: [
+        "app/**/*.css"
+    ],
+    images: 'img/**/*',
+    htmlFiles: 'app/**/*.html'
+};
+
+gulp.task('generateJS', function () {
+
+    stream = mergeStream();
+
+    stream.add(gulp.src(paths.scriptsVendors));
+    stream.add(
+        gulp.src(paths.scriptsApplication)
+            .pipe(plumber())
+            .pipe(babel())
+            .pipe(ngAnnotate())
+            .pipe(uglify())
+    );
+    stream.add(gulp.src(paths.htmlFiles)
+            .pipe(plumber())
+            .pipe(minifyHTML({
+                quotes: true
+            }))
+            .pipe(templateCache({
+                standalone: false,
+                module: "amCompanion",
+                root: "/app/"
+            }))
+    );
+
+    return stream
+        .pipe(concat('script.min.js'))
+        .pipe(gulp.dest('dist/'));
+});
 
 gulp.task('css', function () {
-    gulp.src(['app/**/*.css'])
+
+    stream = mergeStream();
+
+    stream.add(
+        gulp.src(paths.sassFiles)
+            .pipe(plumber())
+            .pipe(concat("all.scss"))
+            .pipe(sass())
+            .pipe(autoprefixer())
+    );
+    stream.add(
+        gulp.src(paths.cssFiles)
+            .pipe(autoprefixer())
+    );
+    return stream
         .pipe(plumber())
-        .pipe(autoprefixer())
         .pipe(cssmin())
-        .pipe(concat('style.min.css'))
-        .pipe(gulp.dest('dist'));
+        .pipe(concat("style.min.css"))
+        .pipe(gulp.dest('dist/'));
+
 });
 
-gulp.task('js', ["partials"], function () {
-    gulp.src(["app/vendors/js/angular.min.js", "app/vendors/js/*.js", "app/app.js", "app/**/*.js"])
-        .pipe(ngAnnotate())
-        .pipe(uglify())
-        .pipe(concat('script.min.js'))
-        .pipe(gulp.dest('dist'));
+gulp.task('serve', ["generateJS", "css"], function () {
+
+    //1. run your script as a server
+    var server = gls.new('server.js');
+    server.start();
+
+    gulp.watch(paths.scriptsApplication, ['generateJS']);
+    gulp.watch(paths.scriptsVendors, ['generateJS']);
+    gulp.watch(paths.htmlFiles, ['generateJS']);
+    gulp.watch(paths.sassFiles, ['css']);
+    gulp.watch(paths.cssFiles, ['css']);
+
+
+    gulp.watch(['dist/**'], function (file) {
+        server.notify.apply(server, [file]);
+    });
+
 });
-
-gulp.task('partials', function () {
-    gulp.src(["app/**/*.html"])
-        .pipe(plumber())
-        .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(ngTemplates({
-            standalone: false,
-            filename: 'templates.js',
-            module: 'amCompanion',
-            path: function (path, base) {
-                return path.replace(base, '/app/');
-            }
-        }))
-        .pipe(gulp.dest('./app/'));
-});
-
-gulp.task('js-lint', function () {
-    return gulp.src(['app/**/*.js', '!app/vendors/**/*.js'])
-        .pipe(plumber())
-        .pipe(jshint())
-        .pipe(jshint.reporter('default', {verbose: true}));
-});
-
-gulp.task('css-lint', function () {
-    return gulp.src(['app/**/*.css', '!app/vendors/**/*.css'])
-        .pipe(plumber())
-        .pipe(csslint('.csslintrc'))
-        .pipe(csslint.reporter());
-});
-
-gulp.task('bower-ressource', function () {
-    return bower().pipe(gulp.dest('tmp/'));
-});
-
-gulp.task('bower-copy', function () {
-    gulp.src([
-        'tmp/**/*.min.js',
-        'tmp/**/angular-i18n/angular-locale_fr-fr.js'
-    ])
-        .pipe(rename({dirname: ''}))
-        .pipe(gulp.dest('app/vendors/js/'));
-});
-
-gulp.task('bower',
-    [
-        'bower-ressource',
-        'bower-copy'
-    ]
-);
-
-gulp.task('watch', function () {
-    gulp.watch('app/**/*.css', ['css', 'css-lint']);
-    gulp.watch('app/**/*.js', ['js', 'js-lint']);
-    gulp.watch('app/**/*.html', ['partials']);
-});
-
-gulp.task('default',
-    [
-        'css',
-        'js',
-        'js-lint',
-        'css-lint'
-    ]
-);
-
-gulp.task('default + watch',
-    [
-        'css',
-        'js',
-        'js-lint',
-        'css-lint',
-        "watch"
-    ]
-);
